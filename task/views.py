67287162848286eb909edd9cf0568d14db1beef3
laouali80 <120820961+laouali80@users.noreply.github.com>
2024-07-02 19:21:09 +0100

    Update views.py

--- task/views.py
+++ task/views.py
@@ -4,6 +4,12 @@ import httpx
 
 # Create your views here.
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 
 @api_view(['GET'])
 def welcome(request):
@@ -12,15 +18,25 @@ def welcome(request):
     }
     return Response(test)
 
+
 @api_view(['GET'])
-async def api(request):
-    visitor_name = request.GET.get("visitor_name", "mark")
+async def test(request):
+    visitor_name = request.query_params.get('visitor_name', 'Mark')
+    
     async with httpx.AsyncClient() as client:
-        ipAddress = await client.get('http://api.ipify.org')
-        response = await client.get(f'http://ip-api.com/json/{ipAddress}').json()
-        temp = await client.get(f"https://api.openweathermap.org/data/2.5/weather?lat={response['lat']}&lon={response['lon']}&units=Metric&appid=bb7a48fca8cf8f19e33795a6d147c742")
-    return {
-        "client_id": ipAddress,
-        "location": response["city"],
-        "greeting": f"Hello, {visitor_name.title()}!, the temperature is {temp["main"]["temp"]} degrees Celcius in {response["city"]}"
-    }
+        ip_address_response = await client.get('http://api.ipify.org')
+        ip_address = ip_address_response.text
+        
+        location_response = await client.get(f'http://ip-api.com/json/{ip_address}')
+        location_data = location_response.json()
+        
+        weather_response = await client.get(
+            f"https://api.openweathermap.org/data/2.5/weather?lat={location_data['lat']}&lon={location_data['lon']}&units=Metric&appid={os.getenv('api_key')}"
+        )
+        weather_data = weather_response.json()
+    
+    return Response({
+        "client_id": ip_address,
+        "location": location_data["city"],
+        "greeting": f"Hello, {visitor_name.title()}!, the temperature is {weather_data['main']['temp']} degrees Celsius in {location_data['city']}"
+    })

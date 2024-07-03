a84ee3c01f53bfccd4d1a160b345cc013c2a9826
Laouali Bachir <laoualibachir2000@gmail.com>
2024-07-03 10:18:58 +0100

    set up everything

--- task/urls.py
+++ task/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.test, name='test'),
-    # path('', views.welcome, name='welcome'),
-    # path('api/hello', views.api, name='api'),
+    path('', views.welcome, name='welcome'),
+    path('api/hello', views.api, name='api'),
 ]
--- task/views.py
+++ task/views.py
@@ -10,40 +10,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 @api_view(['GET'])
-def test(request):
-    return Response(
-        {"etrtr":"fgf"}
+def welcome(request):
+    return redirect('api')
+
+
+
+
+@api_view(['GET'])
+def api(request):
+    visitor_name = request.GET.get('visitor_name', 'Mark')
+    
+    # Get the IP address
+    ip_address_response = requests.get('http://api.ipify.org')
+    ip_address = ip_address_response.text
+    
+    # Get the location data
+    location_response = requests.get(f'http://ip-api.com/json/{ip_address}')
+    location_data = location_response.json()
+    
+    # Get the weather data
+    weather_response = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?lat={location_data['lat']}&lon={location_data['lon']}&units=Metric&appid={os.getenv("api_key")}"
     )
-
-# @api_view(['GET'])
-# def welcome(request):
-#     return redirect('api')
-
-
-
-
-# @api_view(['GET'])
-# def api(request):
-#     visitor_name = request.GET.get('visitor_name', 'Mark')
+    weather_data = weather_response.json()
     
-#     # Get the IP address
-#     ip_address_response = requests.get('http://api.ipify.org')
-#     ip_address = ip_address_response.text
-    
-#     # Get the location data
-#     location_response = requests.get(f'http://ip-api.com/json/{ip_address}')
-#     location_data = location_response.json()
-    
-#     # Get the weather data
-#     weather_response = requests.get(
-#         f"https://api.openweathermap.org/data/2.5/weather?lat={location_data['lat']}&lon={location_data['lon']}&units=Metric&appid={os.getenv("api_key")}"
-#     )
-#     weather_data = weather_response.json()
-    
-#     data = {
-#         "client_id": ip_address,
-#         "location": location_data["city"],
-#         "greeting": f"Hello, {visitor_name.title()}!,the temperature is {weather_data['main']['temp']} degrees Celsius in {location_data['city']}"
-#     }
-#     return Response(data)
+    data = {
+        "client_id": ip_address,
+        "location": location_data["city"],
+        "greeting": f"Hello, {visitor_name.title()}!,the temperature is {weather_data['main']['temp']} degrees Celsius in {location_data['city']}"
+    }
+    return Response(data)
